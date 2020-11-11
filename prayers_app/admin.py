299720e1a8ae364fbfcbe2a74f1df70a55858863
Monica Hong <momo.star@gmail.com>
2020-11-11 02:08:04 -0800

    models on admin

--- prayers_app/admin.py
+++ prayers_app/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from users_app.models import Profile
+from prayers_app.models import Category, Tag, Prayer
 # Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class PrayerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Prayer, PrayerAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tag, TagAdmin)
