5e95da1d8a83e4bbd90710a1ec94fbc9c31e21fb
Monica Hong <momo.star@gmail.com>
2020-11-13 12:11:18 -0800

    click flip

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+    console.log("document ready!");
+      $('.animcard').on('click', function(){
+    console.log(`whelp made it here!`);
+        if ( $(this).hasClass('front') ) {
+          $(this).removeClass('front');
+          $(this).addClass('back');
+        } else {
+          $(this).removeClass('back');
+          $(this).addClass('front');
+        }
+      });
+    });
+
 function cardDisplay() {
     console.log(`whelp made it here!`);
-}
+    // when clicked, 1- do animation to flip tile and 2- display centered as modal
+} 
